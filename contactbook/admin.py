cc8c6b1bd00de67afd4903636c1da0e704cca384
Ashkan Taravati <ashkan.taravati@gmail.com>
2019-01-14 10:33:43 +0330

    registered models on admin site

--- contactbook/admin.py
+++ contactbook/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from contactbook.models import Contact,ContactBook,ContactDetail
 
 # Register your models here.
+admin.site.register(ContactBook)
+admin.site.register(Contact)
+admin.site.register(ContactDetail)
